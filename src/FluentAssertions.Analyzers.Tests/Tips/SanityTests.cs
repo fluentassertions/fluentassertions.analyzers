aca496f185ee78050beb07eaca72984471825b9f
Meir Blachman <meirblachman@gmail.com>
2017-10-23 17:58:04 +0300

    added sanity test cases for #13

--- src/FluentAssertions.Analyzers.Tests/Tips/SanityTests.cs
+++ src/FluentAssertions.Analyzers.Tests/Tips/SanityTests.cs
@@ -14,5 +14,25 @@ namespace FluentAssertions.Analyzers.Tests.Tips
 
             DiagnosticVerifier.VerifyCSharpDiagnosticUsingAllAnalyzers(source);
         }
+
+        [TestMethod]
+        [NotImplemented(Reason = "https://github.com/fluentassertions/fluentassertions.analyzers/issues/13")]
+        public void PropertyOfIndexerShouldBe_ShouldNotThrowException()
+        {
+            const string assertion = "actual[0].Message.Should().Be(\"test\");";
+            var source = GenerateCode.EnumerableAssertion(assertion);
+
+            DiagnosticVerifier.VerifyCSharpDiagnosticUsingAllAnalyzers(source);
+        }
+
+        [TestMethod]
+        [NotImplemented(Reason = "https://github.com/fluentassertions/fluentassertions.analyzers/issues/13")]
+        public void PropertyOfElementAtShouldBe_ShouldNotTriggerDiagnostic()
+        {
+            const string assertion = "actual.ElementAt(0).Message.Should().Be(\"test\");";
+            var source = GenerateCode.EnumerableAssertion(assertion);
+
+            DiagnosticVerifier.VerifyCSharpDiagnosticUsingAllAnalyzers(source);
+        }
     }
 }
