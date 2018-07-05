82fa1cd7f7b0da3977339ef1d03782a458754b6b
Meir Blachman <meirblachman@gmail.com>
2018-07-05 15:49:03 +0300

    nit

--- src/FluentAssertions.Analyzers/Utilities/FluentAssertionsCSharpSyntaxVisitor.cs
+++ src/FluentAssertions.Analyzers/Utilities/FluentAssertionsCSharpSyntaxVisitor.cs
@@ -12,10 +12,10 @@ namespace FluentAssertions.Analyzers
         public IdentifierNameSyntax IdentifierNameVariable { get; private set; }
 
         public ImmutableStack<MemberValidator> AllMembers { get; }
-        public ImmutableStack<MemberValidator> Members { get; private set; }        
-        
+        public ImmutableStack<MemberValidator> Members { get; private set; }
+
         public virtual bool IsValid(ExpressionSyntax expression) => Members.IsEmpty;
-        
+
         public virtual ImmutableDictionary<string, string> ToDiagnosticProperties() => ImmutableDictionary<string, string>.Empty
             .Add(Constants.DiagnosticProperties.VisitorName, GetType().Name)
             .ToImmutableDictionary();
