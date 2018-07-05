bc4f4069bf973f7a572f38a7ad0bbf1700d651dc
Meir Blachman <meirblachman@gmail.com>
2018-07-05 18:52:46 +0300

    cleanup

--- src/FluentAssertions.Analyzers/Tips/Dictionaries/DictionaryAnalyzer.cs
+++ src/FluentAssertions.Analyzers/Tips/Dictionaries/DictionaryAnalyzer.cs
@@ -6,9 +6,6 @@ namespace FluentAssertions.Analyzers
     public abstract class DictionaryAnalyzer : FluentAssertionsAnalyzer
     {
         protected override bool ShouldAnalyzeVariableType(ITypeSymbol type)
-        {
-            var res = type.AllInterfaces.Any(@interface => @interface.Name == "IDictionary");
-            return res;
-        }
+            => type.AllInterfaces.Any(@interface => @interface.Name == "IDictionary");
     }
 }
