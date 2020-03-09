ac28e4b2bb133a1ea764b1c84a8f6e95edc61d8f
Meir Blachman <meirblachman@gmail.com>
2020-03-09 19:50:47 +0200

    add should.equals

---
+++ src/FluentAssertions.Analyzers.Tests/Tips/ShouldEqualsTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions.Analyzers.Tips;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAssertions.Analyzers.Tests.Tips
+{
+    [TestClass]
+    public class ShouldEqualsTests
+    {
+        [TestMethod]
+        [Implemented]
+        public void ShouldEquals_TestAnalyzer()
+            => VerifyCSharpDiagnosticExpressionBody("actual.Should().Equals(expected);");
+
+        [TestMethod]
+        [Implemented]
+        public void ShouldEquals_TestCodeFix()
+            => VerifyCSharpFixCodeBlock(
+                oldSourceAssertion: "actual.Should().Equals(expected);",
+                newSourceAssertion: "actual.Should().Be(expected);");
+
+        private void VerifyCSharpDiagnosticExpressionBody(string sourceAssertion)
+        {
+            var source = GenerateCode.ObjectStatement(sourceAssertion);
+            DiagnosticVerifier.VerifyCSharpDiagnosticUsingAllAnalyzers(source, new DiagnosticResult
+            {
+                Id = ShouldEqualsAnalyzer.DiagnosticId,
+                Message = ShouldEqualsAnalyzer.Message,
+                Locations = new DiagnosticResultLocation[]
+                {
+                    new DiagnosticResultLocation("Test0.cs", 10,13)
+                },
+                Severity = DiagnosticSeverity.Info
+            });
+        }
+
+        private void VerifyCSharpFixCodeBlock(string oldSourceAssertion, string newSourceAssertion)
+        {
+            var oldSource = GenerateCode.ObjectStatement(oldSourceAssertion);
+            var newSource = GenerateCode.ObjectStatement(newSourceAssertion);
+
+            DiagnosticVerifier.VerifyCSharpFix<ShouldEqualsCodeFix, ShouldEqualsAnalyzer>(oldSource, newSource);
+        }
+    }
+}
---
+++ src/FluentAssertions.Analyzers/Tips/ShouldEquals.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentAssertions.Analyzers.Tips
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ShouldEqualsAnalyzer : FluentAssertionsAnalyzer
+    {
+        public const string DiagnosticId = Constants.CodeSmell.ShouldEquals;
+        public const string Category = Constants.CodeSmell.Category;
+        public const string Message = "Use .Should().Equal() instead.";
+
+        protected override DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, DiagnosticSeverity.Info, true);
+        protected override IEnumerable<FluentAssertionsCSharpSyntaxVisitor> Visitors
+        {
+            get
+            {
+                yield return new ShouldEqualsSyntaxVisitor();
+            }
+        }
+
+        public class ShouldEqualsSyntaxVisitor : FluentAssertionsCSharpSyntaxVisitor
+        {
+            public ShouldEqualsSyntaxVisitor() : base(MemberValidator.Should, new MemberValidator("Equals"))
+            {
+            }
+        }
+    }
+
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ShouldEqualsCodeFix)), Shared]
+    public class ShouldEqualsCodeFix : FluentAssertionsCodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(ShouldEqualsAnalyzer.DiagnosticId);
+
+        protected override ExpressionSyntax GetNewExpression(ExpressionSyntax expression, FluentAssertionsDiagnosticProperties properties) 
+            => GetNewExpression(expression, NodeReplacement.Rename("Equals", "Be"));
+    }
+}
