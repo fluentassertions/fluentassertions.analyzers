f60146eadf6748079fdcc4584d94dc1d6e84fa04
Meir Blachman <meirblachman@gmail.com>
2022-01-09 22:20:15 +0200

    fix parent class for CollectionAssert

--- src/FluentAssertions.Analyzers/Tips/MsTest/CollectionAssertAllItemsAreNotNull.cs
+++ src/FluentAssertions.Analyzers/Tips/MsTest/CollectionAssertAllItemsAreNotNull.cs
@@ -1,6 +1,5 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Generic;
