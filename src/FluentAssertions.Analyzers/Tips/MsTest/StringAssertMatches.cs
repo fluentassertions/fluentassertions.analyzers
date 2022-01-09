65295e095e156087439eee3935607b2f69f6694c
Meir Blachman <meirblachman@gmail.com>
2022-01-09 22:21:11 +0200

    fix parent class for StringAssert

--- src/FluentAssertions.Analyzers/Tips/MsTest/StringAssertMatches.cs
+++ src/FluentAssertions.Analyzers/Tips/MsTest/StringAssertMatches.cs
@@ -1,6 +1,5 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Generic;
@@ -10,7 +9,7 @@ using System.Composition;
 namespace FluentAssertions.Analyzers
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class StringAssertMatchesAnalyzer : MsTestAnalyzer
+    public class StringAssertMatchesAnalyzer : MsTestStringAssertAnalyzer
     {
         public const string DiagnosticId = Constants.Tips.MsTest.StringAssertMatches;
         public const string Category = Constants.Tips.Category;
