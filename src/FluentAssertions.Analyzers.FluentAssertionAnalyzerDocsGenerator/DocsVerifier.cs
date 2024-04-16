622c55f6a94f1ed31f8e046dc32f2f239e7d1bed
Meir Blachman <meirblachman@gmail.com>
2024-04-16 15:15:05 +0300

    cleanup

--- src/FluentAssertions.Analyzers.FluentAssertionAnalyzerDocsGenerator/DocsVerifier.cs
+++ src/FluentAssertions.Analyzers.FluentAssertionAnalyzerDocsGenerator/DocsVerifier.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,18 @@ public class DocsVerifier
     {
         var compilation = await FluentAssertionAnalyzerDocsUtils.GetFluentAssertionAnalyzerDocsCompilation();
         var issues = new StringBuilder();
+
+        void ValidateAssertions(IEnumerable<ExpressionStatementSyntax> oldAssertions, ExpressionStatementSyntax newAssertion, MethodDeclarationSyntax method, string className)
+        {
+            foreach (var oldAssertion in oldAssertions)
+            {
+                if (!oldAssertion.WithoutTrivia().IsEquivalentTo(newAssertion.WithoutTrivia()))
+                {
+                    issues.AppendLine($"[{className}] {method.Identifier} - actual: {oldAssertion.ToFullString()} expected: {newAssertion.ToFullString()}");
+                }
+            }
+        }
+
         foreach (var tree in compilation.SyntaxTrees.Where(t => t.FilePath.EndsWith("Tests.cs")))
         {
             Console.WriteLine($"File: {Path.GetFileName(tree.FilePath)}");
@@ -27,61 +40,24 @@ public class DocsVerifier
             {
                 Console.WriteLine($"### scenario: {method.Identifier}");
 
-                var oldAssertionComment = method.DescendantTrivia().First(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// old assertion:"));
-                var newAssertionComment = method.DescendantTrivia().First(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// new assertion:"));
+                var (oldAssertions, newAssertion) = GetAssertionsFromMethod(method);
 
-                var statements = method.Body.Statements.OfType<ExpressionStatementSyntax>();
-
-                var oldAssertions = statements.Where(x => x.Span.CompareTo(oldAssertionComment.Span) > 0 && x.Span.CompareTo(newAssertionComment.Span) < 0);
-                var newAssertion = statements.Single(x => x.Span.CompareTo(newAssertionComment.Span) > 0);
-
-                foreach (var oldAssertion in oldAssertions)
-                {
-                    if (!oldAssertion.WithoutTrivia().IsEquivalentTo(newAssertion.WithoutTrivia()))
-                    {
-                        issues.AppendLine($"[{tree.FilePath.Split('\\')[^1]}:{oldAssertion.GetLocation().GetLineSpan().Span.Start}] {method.Identifier} - actual: {oldAssertion.ToFullString()} expected: {newAssertion.ToFullString()}");
-                    }
-                }
+                ValidateAssertions(oldAssertions, newAssertion, method, tree.FilePath.Split('\\')[^1]);
 
                 if (methodsMap.TryGetValue($"{method.Identifier.Text}_Failure", out var methodFailure))
                 {
-                    var oldAssertionFailureComment = methodFailure.DescendantTrivia().First(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// old assertion:"));
-                    var newAssertionFailureComment = methodFailure.DescendantTrivia().First(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// new assertion:"));
+                    var (oldAssertionsFailure, newAssertionFailure) = GetAssertionsFromMethod(methodFailure);
 
-                    var statementsFailure = methodFailure.Body.Statements.OfType<ExpressionStatementSyntax>();
-
-                    var oldAssertionsFailure = statementsFailure.Where(x => x.Span.CompareTo(oldAssertionFailureComment.Span) > 0 && x.Span.CompareTo(newAssertionFailureComment.Span) < 0);
-                    var newAssertionFailure = statementsFailure.Single(x => x.Span.CompareTo(newAssertionFailureComment.Span) > 0);
-
-                    foreach (var oldAssertionFailure in oldAssertionsFailure)
-                    {
-                        if (!oldAssertionFailure.WithoutTrivia().IsEquivalentTo(newAssertionFailure.WithoutTrivia()))
-                        {
-                            issues.AppendLine($"[{tree.FilePath.Split('\\')[^1]}:{oldAssertionFailure.GetLocation().GetLineSpan().Span.Start}] {methodFailure.Identifier} - actual: {oldAssertionFailure.ToFullString()} expected: {newAssertionFailure.ToFullString()}");
-                        }
-                    }
+                    ValidateAssertions(oldAssertionsFailure, newAssertionFailure, methodFailure, tree.FilePath.Split('\\')[^1]);
                 }
 
                 if (methodsMap.TryGetValue($"{method.Identifier.Text}_Failure_OldAssertion", out var testWithFailureOldAssertion)
                 && methodsMap.TryGetValue($"{method.Identifier.Text}_Failure_NewAssertion", out var testWithFailureNewAssertion))
                 {
-                    var oldAssertionFailureComment = testWithFailureOldAssertion.DescendantTrivia().First(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// old assertion:"));
-                    var newAssertionFailureComment = testWithFailureNewAssertion.DescendantTrivia().First(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// new assertion:"));
+                    var (oldAssertionsFailure, _) = GetAssertionsFromMethod(testWithFailureOldAssertion);
+                    var (_, newAssertionFailure) = GetAssertionsFromMethod(testWithFailureNewAssertion);
 
-                    var statementsFailureOldAssertion = testWithFailureOldAssertion.Body.Statements.OfType<ExpressionStatementSyntax>();
-                    var statementsFailureNewAssertion = testWithFailureNewAssertion.Body.Statements.OfType<ExpressionStatementSyntax>();
-
-                    var oldAssertionsFailure = statementsFailureOldAssertion.Where(x => x.Span.CompareTo(oldAssertionFailureComment.Span) > 0 && x.Span.CompareTo(newAssertionFailureComment.Span) < 0);
-                    var newAssertionFailure = statementsFailureNewAssertion.Single(x => x.Span.CompareTo(newAssertionFailureComment.Span) > 0);
-
-                    foreach (var oldAssertionFailure in oldAssertionsFailure)
-                    {
-                        if (!oldAssertionFailure.WithoutTrivia().IsEquivalentTo(newAssertionFailure.WithoutTrivia()))
-                        {
-                            issues.AppendLine($"[{tree.FilePath.Split('\\')[^1]}:{oldAssertionFailure.GetLocation().GetLineSpan().Span.Start}] {testWithFailureOldAssertion.Identifier} - actual: {oldAssertionFailure.ToFullString()} expected: {newAssertionFailure.ToFullString()}");
-                        }
-                    }
-                
+                    ValidateAssertions(oldAssertionsFailure, newAssertionFailure, testWithFailureOldAssertion, tree.FilePath.Split('\\')[^1]);
                 }
             }
         }
@@ -90,5 +66,18 @@ public class DocsVerifier
         {
             throw new Exception(issues.ToString());
         }
+    }
+
+    (IEnumerable<ExpressionStatementSyntax> oldAssertions, ExpressionStatementSyntax newAssertion) GetAssertionsFromMethod(MethodDeclarationSyntax method)
+    {
+        var oldAssertionComment = method.DescendantTrivia().FirstOrDefault(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// old assertion:"));
+        var newAssertionComment = method.DescendantTrivia().FirstOrDefault(x => x.IsKind(SyntaxKind.SingleLineCommentTrivia) && x.ToString().Equals("// new assertion:"));
+
+        var statements = method.Body.Statements.OfType<ExpressionStatementSyntax>();
+
+        var oldAssertions = statements.Where(x => x.Span.CompareTo(oldAssertionComment.Span) > 0 && x.Span.CompareTo(newAssertionComment.Span) < 0);
+        var newAssertion = statements.SingleOrDefault(x => x.Span.CompareTo(newAssertionComment.Span) > 0);
+
+        return (oldAssertions, newAssertion);
     }
 }
