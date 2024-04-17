4fafffca943fa083c072c2daadf5f525336586e5
Meir Blachman <meirblachman@gmail.com>
2024-04-17 15:11:57 +0300

    docs: mstest support for IsNull, IsNotNull, IsInstanceOfType, IsNotInstanceOfType

--- src/FluentAssertions.Analyzers.FluentAssertionAnalyzerDocs/MsTestAnalyzerTests.cs
+++ src/FluentAssertions.Analyzers.FluentAssertionAnalyzerDocs/MsTestAnalyzerTests.cs
@@ -75,5 +75,158 @@ public class MsTestAnalyzerTests
         // new assertion:
         flag.Should().BeFalse();
     }
-    
+
+    [TestMethod]
+    public void ObjectAssertIsNull()
+    {
+        // arrange
+        object obj = null;
+
+        // old assertion:
+        Assert.IsNull(obj);
+
+        // new assertion:
+        obj.Should().BeNull();
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ObjectAssertIsNull_Failure_OldAssertion()
+    {
+        // arrange
+        var obj = new object();
+
+        // old assertion:
+        Assert.IsNull(obj);
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ObjectAssertIsNull_Failure_NewAssertion()
+    {
+        // arrange
+        var obj = new object();
+
+        // new assertion:
+        obj.Should().BeNull();
+    }
+
+    [TestMethod]
+    public void ObjectAssertIsNotNull()
+    {
+        // arrange
+        var obj = new object();
+
+        // old assertion:
+        Assert.IsNotNull(obj);
+
+        // new assertion:
+        obj.Should().NotBeNull();
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ObjectAssertIsNotNull_Failure_OldAssertion()
+    {
+        // arrange
+        object obj = null;
+
+        // old assertion:
+        Assert.IsNotNull(obj);
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ObjectAssertIsNotNull_Failure_NewAssertion()
+    {
+        // arrange
+        object obj = null;
+
+        // new assertion:
+        obj.Should().NotBeNull();
+    }
+
+    [TestMethod]
+    public void ReferenceTypeAssertIsInstanceOfType()
+    {
+        // arrange
+        var obj = new List<object>();
+
+        // old assertion:
+        Assert.IsInstanceOfType(obj, typeof(List<object>));
+        Assert.IsInstanceOfType<List<object>>(obj);
+
+        // new assertion:
+        obj.Should().BeOfType<List<object>>();
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ReferenceTypeAssertIsInstanceOfType_Failure_OldAssertion_0()
+    {
+        // arrange
+        var obj = new List<int>();
+
+        // old assertion:
+        Assert.IsInstanceOfType(obj, typeof(List<object>));
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ReferenceTypeAssertIsInstanceOfType_Failure_OldAssertion_1()
+    {
+        // arrange
+        var obj = new List<int>();
+
+        // old assertion:
+        Assert.IsInstanceOfType<List<object>>(obj);
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ReferenceTypeAssertIsInstanceOfType_Failure_NewAssertion()
+    {
+        // arrange
+        var obj = new List<int>();
+
+        // new assertion:
+        obj.Should().BeOfType<List<object>>();
+    }
+
+    [TestMethod]
+    public void ReferenceTypeAssertIsNotInstanceOfType()
+    {
+        // arrange
+        var obj = new List<int>();
+
+        // old assertion:
+        Assert.IsNotInstanceOfType(obj, typeof(List<object>));
+        Assert.IsNotInstanceOfType<List<object>>(obj);
+
+        // new assertion:
+        obj.Should().NotBeOfType<List<object>>();
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ReferenceTypeAssertIsNotInstanceOfType_Failure_OldAssertion_0()
+    {
+        // arrange
+        var obj = new List<object>();
+
+        // old assertion:
+        Assert.IsNotInstanceOfType(obj, typeof(List<object>));
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ReferenceTypeAssertIsNotInstanceOfType_Failure_OldAssertion_1()
+    {
+        // arrange
+        var obj = new List<object>();
+
+        // old assertion:
+        Assert.IsNotInstanceOfType<List<object>>(obj);
+    }
+
+    [TestMethod, ExpectedException(typeof(AssertFailedException))]
+    public void ReferenceTypeAssertIsNotInstanceOfType_Failure_NewAssertion()
+    {
+        // arrange
+        var obj = new List<object>();
+
+        // new assertion:
+        obj.Should().NotBeOfType<List<object>>();
+    }
 }
