ef28e6bf7ba2d0d00cbfac1a67e44248c400051f
Meir Blachman <meirblachman@gmail.com>
2024-05-15 11:09:41 +0300

    fix docs script

--- scripts/run-docs-tests.ps1
+++ scripts/run-docs-tests.ps1
@@ -12,9 +12,15 @@ if ($FormatAndExecuteTestsAgain) {
     Push-Location src
     Push-Location FluentAssertions.Analyzers.FluentAssertionAnalyzerDocs
 
+    $i = 1;
     do {
+        Write-Host "formatting code... - Iteration $i"
         $out = dotnet format analyzers --diagnostics FAA0001 FAA0003 FAA0004 --severity info --verbosity normal
-    } while ($out -ccontains "Unable to fix FAA0004. Code fix NunitCodeFixProvider didn't return a Fix All action")
+
+        Write-Host $out
+
+        $i++
+    } while ($out -cnotcontains "Unable to fix FAA0004. Code fix NunitCodeFixProvider didn't return a Fix All action")
 
     Pop-Location
     Pop-Location
