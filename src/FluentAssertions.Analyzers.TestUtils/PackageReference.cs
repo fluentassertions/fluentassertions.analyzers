580d73546f4994feb2d41c4b4879ab8b522a8476
Meir Blachman <meirblachman@gmail.com>
2024-05-20 11:43:29 +0300

    download dependencies on startup

--- src/FluentAssertions.Analyzers.TestUtils/PackageReference.cs
+++ src/FluentAssertions.Analyzers.TestUtils/PackageReference.cs
@@ -1,17 +1,34 @@
+using System.Collections.Generic;
+
 public class PackageReference
 {
+    private static readonly List<PackageReference> _allDependencies = new();
+
     public string Name { get; }
     public string Version { get; }
     public string Path { get; }
 
-    public PackageReference(string name, string version, string path) => (Name, Version, Path) = (name, version, path);
 
-    public static PackageReference FluentAssertions_6_12_0 { get; } = FluentAssertions("6.12.0");
+    private PackageReference(string name, string version, string path)
+    {
+        Name = name;
+        Version = version;
+        Path = path;
 
+        _allDependencies.Add(this);
+    }
+
+    public static PackageReference FluentAssertions_6_12_0 { get; } = new("FluentAssertions", "6.12.0", "lib/netstandard2.0/");
     public static PackageReference MSTestTestFramework_3_1_1 { get; } = new("MSTest.TestFramework", "3.1.1", "lib/netstandard2.0/");
     public static PackageReference XunitAssert_2_5_1 { get; } = new("xunit.assert", "2.5.1", "lib/netstandard1.1/");
     public static PackageReference Nunit_3_14_0 { get; } = new("NUnit", "3.14.0", "lib/netstandard2.0/");
     public static PackageReference Nunit_4_0_1 { get; } = new("NUnit", "4.0.1", "lib/net6.0/");
 
-    public static PackageReference FluentAssertions(string version) => new("FluentAssertions", version, "lib/netstandard2.0/");
+    internal static PackageReference NETStandard2_0 = new("NETStandard.Library", "2.0.3", "build/netstandard2.0/ref/");
+    internal static PackageReference NETStandard2_1 = new("NETStandard.Library.Ref", "2.1.0", "ref/netstandard2.1/ref/");
+    internal static PackageReference DotNet6 = new("Microsoft.NETCore.App.Ref", "6.0.25", "ref/net6.0/");
+    internal static PackageReference DotNet7 = new("Microsoft.NETCore.App.Ref", "7.0.14", "ref/net7.0/");
+    internal static PackageReference DotNet8 = new("Microsoft.NETCore.App.Ref", "8.0.0", "ref/net8.0/");
+
+    internal static IEnumerable<PackageReference> AllDependencies => _allDependencies;
 }
