59c4a68001b9e8a6da404ec6822c469bc6eb7d3c
Meir Blachman <meirblachman@gmail.com>
2024-05-29 08:36:18 +0300

    chore: update tests project to dotnet 8

--- src/FluentAssertions.Analyzers.TestUtils/PackageReference.cs
+++ src/FluentAssertions.Analyzers.TestUtils/PackageReference.cs
@@ -26,9 +26,9 @@ public class PackageReference
 
     internal static PackageReference NETStandard2_0 = new("NETStandard.Library", "2.0.3", "build/netstandard2.0/ref/");
     internal static PackageReference NETStandard2_1 = new("NETStandard.Library.Ref", "2.1.0", "ref/netstandard2.1/ref/");
-    internal static PackageReference DotNet6 = new("Microsoft.NETCore.App.Ref", "6.0.25", "ref/net6.0/");
-    internal static PackageReference DotNet7 = new("Microsoft.NETCore.App.Ref", "7.0.14", "ref/net7.0/");
-    internal static PackageReference DotNet8 = new("Microsoft.NETCore.App.Ref", "8.0.0", "ref/net8.0/");
+    internal static PackageReference DotNet6 = new("Microsoft.NETCore.App.Ref", "6.0.31", "ref/net6.0/");
+    internal static PackageReference DotNet7 = new("Microsoft.NETCore.App.Ref", "7.0.20", "ref/net7.0/");
+    internal static PackageReference DotNet8 = new("Microsoft.NETCore.App.Ref", "8.0.6", "ref/net8.0/");
 
     internal static IEnumerable<PackageReference> AllDependencies => _allDependencies;
 }
