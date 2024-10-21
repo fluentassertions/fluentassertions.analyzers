445c91b08ae09c114d4a12a4436a0ad94fad2a80
Meir Blachman <meirblachman@gmail.com>
2024-10-21 11:23:17 +0300

    bugfix: improve CollectionShouldNotHaveSameCount_CountShouldNotBeOtherCollectionCount detection

--- src/FluentAssertions.Analyzers/Tips/FluentAssertionsAnalyzer.cs
+++ src/FluentAssertions.Analyzers/Tips/FluentAssertionsAnalyzer.cs
@@ -408,7 +408,7 @@ public partial class FluentAssertionsAnalyzer : DiagnosticAnalyzer
                         {
                             // TODO: add support for Enumerable.LongCount
                             case nameof(Enumerable.Count) when IsEnumerableMethodWithoutArguments(invocationBeforeShould, metadata):
-                                if (assertion.Arguments[0].HasFirstDescendentInvocation(nameof(Enumerable.Count)))
+                                if (assertion.TryGetSingleArgumentAs<IInvocationOperation>(out var assertionInvocation) && assertionInvocation.TargetMethod.Name is nameof(Enumerable.Count))
                                 {
                                     context.ReportDiagnostic(CreateDiagnostic(assertion, DiagnosticMetadata.CollectionShouldNotHaveSameCount_CountShouldNotBeOtherCollectionCount));
                                 }
